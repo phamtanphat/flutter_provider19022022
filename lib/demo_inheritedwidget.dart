abc34899bbb30394481fd33410296a567eaee8b3
phamtanphat <phatdroid94@gmail.com>
2022-03-15 20:38:21 +0700

    ongba

--- lib/demo_inheritedwidget.dart
+++ lib/demo_inheritedwidget.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-class DemoInheritedWidget extends StatefulWidget {
+import 'package:flutter/widgets.dart';
 
+class DemoInheritedWidget extends StatefulWidget {
   @override
   State<DemoInheritedWidget> createState() => _DemoInheritedWidgetState();
 }
@@ -12,7 +14,52 @@ class _DemoInheritedWidgetState extends State<DemoInheritedWidget> {
       appBar: AppBar(
         title: Text("Demo Inherited Widget"),
       ),
-      body: Container(),
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        child: Ongba(),
+      ),
     );
+  }
+}
+
+//Ong ba : state full
+class Ongba extends StatefulWidget {
+  String data = "Hello";
+
+  @override
+  State<Ongba> createState() => _OngbaState();
+}
+
+class _OngbaState extends State<Ongba> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Ong ba widget",
+              style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold))
+        ],
+      ),
+    );
+  }
+}
+
+// Cha me : stateless
+class Chame extends StatelessWidget {
+  const Chame({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+// Concai : stateless
+class Concai extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
